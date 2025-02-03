ee250e2d6f62c37a4260a341e22d4e3962c8d6cd
David <dmaynor488@gmail.com>
2025-02-02 21:03:40 -0600

    instalando la mejor version disponible de la API

--- src/main/java/org/Pet/PetModule.java
+++ src/main/java/org/Pet/PetModule.java
@@ -1,21 +1,75 @@
 package org.Pet;
 
-import com.github.manolo8.darkbot.core.manager.HeroManager;
-import com.github.manolo8.darkbot.extensions.Plugin;
-import com.github.manolo8.darkbot.extensions.PluginInfo;
-import com.sun.source.util.JavacTask;
+import com.github.manolo8.darkbot.core.manager.PetManager;
+import eu.darkbot.api.config.ConfigSetting;
+import eu.darkbot.api.config.annotations.Number;
+import eu.darkbot.api.extensions.Behavior;
+import eu.darkbot.api.extensions.Configurable;
+import eu.darkbot.api.managers.HeroAPI;
 
-public class PetModule implements Plugin {
+import java.lang.reflect.Method;
 
-    private HeroManager heroManager;
 
-    @Override
-    public String getName() {
-        return "";
+public class PetModule implements Behavior, Configurable<PetModule.Config> {
+
+    private final HeroAPI hero;
+    private final PetManager petManager;
+    private Config config;
+
+    public static class Config {
+        public  boolean useDefaultModule = true;
+
+        public boolean useComboModule = false;
+        public @Number(min = 1, max = 100) int minHpCombo = 50;
+
+        public boolean useHpLink = false;
+        public @Number(min = 1, max = 100) int maxHpLink = 40;
+
+        public boolean useRepairModule = false;
+        public @Number(min = 1, max = 100) int minRepairModule = 30;
+
+        public boolean useDefenseModule = false;
+        public @Number(min = 1, max = 100) int minDefenseModule = 25;
+
+        public boolean useLlamadaExpiratoria = false;
+        public @Number(min = 1, max = 100) int minLlamadaExpiratoria = 20;
+
+    }
+
+    public PetModule(HeroAPI hero, PetManager petManager) {
+        this.hero = hero;
+        this.petManager = petManager;
     }
 
     @Override
-    public void init(JavacTask task, String... args) {
+    public void setConfig(ConfigSetting<Config> configSetting) {
+        this.config = configSetting.getValue();
+    }
+
+
+    @Override
+    public void onTickBehavior() {
+        managePetModule();
+    }
+
+    private void managePetModule() {
+        if(!petManager.isActive() ) return;
+
+        if(config.useComboModule && hero.getHealth().hpPercent() <= config.minHpCombo ) {
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        
+
+        
+        for (Object gear : petManager1.getGears()) {
+            System.out.println(gear);
+        }
+
 
     }
 }
