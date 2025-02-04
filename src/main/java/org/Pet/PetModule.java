252207d8454379a4a1b32ec2585a733cb4516ac7
David <dmaynor488@gmail.com>
2025-02-04 17:18:48 -0600

    arreglando las confusiones con los modulos

--- src/main/java/org/Pet/PetModule.java
+++ src/main/java/org/Pet/PetModule.java
@@ -1,5 +1,6 @@
 package org.Pet;
 
+import com.github.manolo8.darkbot.backpage.entities.ShipInfo;
 import com.github.manolo8.darkbot.config.types.Option;
 import com.github.manolo8.darkbot.core.manager.PetManager;
 import eu.darkbot.api.config.ConfigSetting;
@@ -8,8 +9,11 @@ import eu.darkbot.api.config.annotations.Number;
 import eu.darkbot.api.extensions.Behavior;
 import eu.darkbot.api.extensions.Configurable;
 import eu.darkbot.api.extensions.Feature;
+import eu.darkbot.api.game.entities.Entity;
+import eu.darkbot.api.game.entities.Ship;
 import eu.darkbot.api.game.enums.PetGear;
 import eu.darkbot.api.managers.HeroAPI;
+import eu.darkbot.api.managers.PetAPI;
 import eu.darkbot.api.utils.ItemNotEquippedException;
 
 
@@ -17,7 +21,9 @@ import eu.darkbot.api.utils.ItemNotEquippedException;
 public class PetModule implements Behavior, Configurable<PetModule.Config> {
 
     private final HeroAPI hero;
+    private final Ship ship;
     private final PetManager petManager;
+    private PetAPI petAPI;
     private Config config;
     private PetGear lastGear;
 
@@ -49,9 +55,65 @@ public class PetModule implements Behavior, Configurable<PetModule.Config> {
 
     }
 
-    public PetModule(HeroAPI hero, PetManager petManager) {
+    public PetModule(HeroAPI hero, Ship ship, PetManager petManager) {
         this.hero = hero;
+        this.ship = ship;
         this.petManager = petManager;
+    }
+
+    private void activeModuleDefense() throws ItemNotEquippedException {
+        Entity target = hero.getTarget();
+
+        if(target instanceof Ship){
+            Ship ship = (Ship) target;
+
+            if(ship.getEntityInfo().isEnemy()){
+                if(petAPI.getGear() != PetGear.GUARD){
+                    petAPI.setGear(PetGear.GUARD);
+                }
+            }
+        }
+    }
+
+    private void useComboModule() throws ItemNotEquippedException {
+        if(config.useComboModule && hero.getHealth().hpPercent() <= config.min_Hp_Combo){
+            petAPI.setGear(PetGear.COMBO_REPAIR);
+        }
+    }
+
+    private void useHPLink() throws ItemNotEquippedException {
+        if (config.useHpLink && hero.getHealth().hpPercent() <= config.min_Hp_Link) {
+            petManager.setGear(PetGear.HP_LINK);
+        }
+
+    }
+
+    private void useRepairModule() throws ItemNotEquippedException {
+        if (config.useRepairModule && petAPI.getHealth().hpPercent() <= config.min_Repair_Module ) {
+            petManager.setGear(PetGear.REPAIR);
+        }
+    }
+
+    private void useLlamaExpiratoria() throws ItemNotEquippedException {
+        if (config.useLlamaExpiratoria && getSyncPlayerHealth() <= config.min_Llama_Expiratoria ){
+            petManager.setGear(PetGear.SACRIFICIAL);
+        }
+    }
+
+    private void managePetModule() throws ItemNotEquippedException {
+        if(!petManager.isActive() ) return;
+        PetGear selectedGear = config.defaultModule;
+
+        activeModuleDefense();
+        useComboModule();
+        useHPLink();
+        useRepairModule();
+        useLlamaExpiratoria();
+
+    }
+
+    private int getSyncPlayerHealth() {
+        return 50;
     }
 
     @Override
@@ -69,33 +131,4 @@ public class PetModule implements Behavior, Configurable<PetModule.Config> {
         }
     }
 
-    private void managePetModule() throws ItemNotEquippedException {
-        if(!petManager.isActive() ) return;
-        PetGear selectedGear = config.defaultModule;
-
-        if(config.useComboModule && hero.getHealth().hpPercent() <= config.min_Hp_Combo ) {
-            petManager.setGear(PetGear.COMBO_REPAIR );
-        }
-        else if (config.useHpLink && hero.getHealth().hpPercent() <= config.min_Hp_Link) {
-            petManager.setGear(PetGear.HP_LINK);
-        }
-        else if (config.useRepairModule && hero.getHealth().hpPercent() <= config.min_Repair_Module ) {
-            petManager.setGear(PetGear.REPAIR);
-        }
-        else if (config.useDefenseModule && hero.getHealth().hpPercent() <= config.min_Defense_Module ) {
-            petManager.setGear(PetGear.COMBO_GUARD);
-        }
-        else if (config.useLlamaExpiratoria && getSyncPlayerHealth() <= config.min_Llama_Expiratoria ){
-            petManager.setGear(PetGear.SACRIFICIAL);
-        }
-
-        if (lastGear != selectedGear) {
-            petManager.setGear(selectedGear);
-            lastGear = selectedGear;
-        }
-    }
-
-    private int getSyncPlayerHealth() {
-        return 50;
-    }
 }
